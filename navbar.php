39012c99f4c1802936355f706764d4581351103f
Sal03 <74775056+Sal03@users.noreply.github.com>
2021-03-21 23:33:51 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-light bg-dark">
+    <a href="index.php" class="navbar-brand" style="color: white;"><b> Bank of India</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navabar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a href="index.php" class="nav-link" style="color: red ;"><b>Home</b></a>
+            </li>
+            <li class="nav-item">
+                <a href="createuser.php" class="nav-link" style="color: red ;"><b>Create User</b></a>
+            </li>
+            <li class="nav-item">
+                <a href="transfermoney.php" class="nav-link" style="color: red ;"><b>Transfer Money</b></a>
+            </li>
+            <li class="nav-item">
+                <a href="transactionhistory.php" class="nav-link" style="color: red ;"><b>Transaction History</b></a>
+            </li>
+        </ul>
+    </div>
+</nav>
